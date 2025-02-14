1c021729ef4de1c0d8241990a6da500b25b033ab
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-14 16:37:58 +0900

    Update 조건에 맞게 수열 변환하기 1.js
    added extra solution

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,3 +1,9 @@
 function solution(arr) {
+    return arr.map(v=>{if(v>=50 &&v%2==0) {return v/=2} else if(v<50 && v%2 ==1) {return v*=2} else return v});
+}
+
+//or
+
+function solution(arr) {
     return arr.map(v=>v>=50&&v%2==0?v/=2:v<50&&v%2==1?v*=2:v);
 }
