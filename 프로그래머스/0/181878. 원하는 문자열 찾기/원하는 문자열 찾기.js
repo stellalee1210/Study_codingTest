d8d483999a2562a8dc6d53abae0ba2a62a07f24e
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-14 16:21:35 +0900

    Update 원하는 문자열 찾기.js
    added extra solution

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,3 +1,11 @@
 function solution(myString, pat) {
     return myString.toLowerCase().includes(pat.toLowerCase())?1:0;
 }
+
+
+//or
+
+
+function solution(myString, pat) {
+    return +(myString.toLowerCase().includes(pat.toLowerCase()));
+}
