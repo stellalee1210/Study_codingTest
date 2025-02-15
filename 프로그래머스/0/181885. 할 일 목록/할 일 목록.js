0a8cd6045a40859dd1343043da30cdce066acb91
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-15 21:31:01 +0900

    Update 할 일 목록.js
    update

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,3 +1,16 @@
 function solution(todo_list, finished) {
+    let newList = [];
+    for(let i=0; i<=finished.length; i++){
+        if(finished[i] === false){
+            newList.push(todo_list[i])
+        }
+    }
+    return newList;
+}
+
+
+//or
+
+function solution(todo_list, finished) {
     return todo_list.filter((e,i) => !finished[i]);
 }
