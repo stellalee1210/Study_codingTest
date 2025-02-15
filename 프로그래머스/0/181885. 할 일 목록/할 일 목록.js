9927c0b0b1f8f59e66da0e630d1adf8890b089d6
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-15 21:30:00 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,16 +1,3 @@
 function solution(todo_list, finished) {
-    let newList = [];
-    for(let i=0; i<=finished.length; i++){
-        if(finished[i] === false){
-            newList.push(todo_list[i])
-        }
-    }
-    return newList;
-}
-
-//or
-
-function solution(todo_list, finished) {
-    var answer = [];
     return todo_list.filter((e,i) => !finished[i]);
 }
