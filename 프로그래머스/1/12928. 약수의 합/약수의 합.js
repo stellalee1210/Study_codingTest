d1295887b2cc59ecf3063934b5c3b1902b8d19e5
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-17 22:02:20 +0900

    Update 약수의 합.js
    added extra solution

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,4 +1,20 @@
 function solution(n) {
+    let arr = [];
+    const sqrtNum = Math.sqrt(n);
+    for(let i=1; i<=sqrtNum; i++){
+        if(n%i ===0){
+            arr.push(i);
+            if(i*i != n) arr.push(n/i);
+        }
+    }
+    return arr.reduce((acc, cur) => acc+cur, 0);
+}
+
+
+//or
+
+
+function solution(n) {
     let sum = 0;
     for(let i=1; i<=Math.sqrt(n); i++){
         if(n%i ===0){
