a2d85ff6d6ca8fcf27f2645ef000ad2eb25d5df3
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-18 15:34:01 +0900

    Update 두 정수 사이의 합.js
    added extra solution

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -6,3 +6,12 @@ function solution(a, b) {
     for(let i =min; i<=max; i++) sum+= i;
     return sum;
 }
+
+
+//or
+
+
+function adder(a, b, s = 0){
+  for (var i = Math.min(a, b); i <= Math.max(a, b); i++) s += i;
+  return s;
+}
