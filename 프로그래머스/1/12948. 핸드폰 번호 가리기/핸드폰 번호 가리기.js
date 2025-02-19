592f8a5f5bfc83cd7475d9b674e9fefc47cb8a35
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-19 19:45:26 +0900

    Update 핸드폰 번호 가리기.js
    added extra solution

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,10 @@
 function solution(phone_number) {
     return phone_number.slice(phone_number.length -4).padStart(phone_number.length, '*');
 }
+
+
+//정규식 풀이
+
+function hide_numbers(s){
+  return s.replace(/\d(?=\d{4})/g, "*");
+}
