aae6788dbb68b23d48a41f5653011272a5a09bc1
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-19 19:13:22 +0900

    Update 제일 작은 수 제거하기.js
    added extra solution

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -3,3 +3,12 @@ function solution(arr) {
     arr.splice(idx,1);
     return arr.length === 0? [-1] : arr;
 }
+
+//or
+
+
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
+}
