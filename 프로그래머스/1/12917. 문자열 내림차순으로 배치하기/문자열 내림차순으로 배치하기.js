04e9545f41c1f05efe00af5ebf84a8b60a726bf4
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-19 20:13:31 +0900

    Update 문자열 내림차순으로 배치하기.js
    added extra solution

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,3 +1,14 @@
 function solution(s) {
     return Array.from(s).sort((a,b) => b > a ? 1 : -1).join('');
 }
+
+//or
+
+
+function solution(s) {
+  return s
+    .split("")
+    .sort()
+    .reverse()
+    .join("");
+}
