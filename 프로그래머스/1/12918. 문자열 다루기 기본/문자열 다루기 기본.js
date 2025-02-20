55b6ffe50157fd6c0dc6e8194f06ed0ec4018f60
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-20 10:36:12 +0900

    Update 문자열 다루기 기본.js
    added extra solution

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -3,3 +3,11 @@ function solution(s) {
     return (len === 4 || len === 6) ? 
         (s.replaceAll(/[0-9]/g, "") === "" ? true : false) : false;
 }
+
+
+//or 
+
+function alpha_string46(s){
+    var regex = /^\d{6}$|^\d{4}$/;
+  return regex.test(s);
+}
