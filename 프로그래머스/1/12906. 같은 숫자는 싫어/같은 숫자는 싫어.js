356b167e536fffd0656e893980675ce5c966619d
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-20 11:17:58 +0900

    Update 같은 숫자는 싫어.js
    added extra solution

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,4 +1,17 @@
 function solution(arr)
 {
+    let answer = [];
+    for(let i = 0; i<arr.length; i++){
+        if(!i) answer.push(arr[i]);
+        else if(arr[i-1] !== arr[i]) answer.push(arr[i]);
+    }
+    return answer;
+}
+
+//or
+
+
+function solution(arr)
+{
     return arr.filter((val,index) => val != arr[index+1]);
 }
