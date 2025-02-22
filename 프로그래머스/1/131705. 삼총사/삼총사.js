7909890e8d11d9b6a07cf891de940e318e6dfeb0
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-22 10:56:22 +0900

    [level 1] Title: 삼총사, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -4,7 +4,7 @@ function solution(number) {
     for(let i=0; i<number.length-2;i++){
         for(let j = i+1; j<number.length-1; j++){
             for(let k = j+1; k<number.length;k++){
-                if(number[i] + number[j] + number[k] === 0) answer++;
+                answer += number[i] + number[j] + number[k] === 0? 1:0;
             }
         }
     }
