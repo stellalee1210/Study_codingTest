220d949d19cb0525708820b3674069c8642b6866
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-02-26 17:41:29 +0900

    Update 소수 만들기.js
    added extra solution

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,3 +1,4 @@
+//에라스토스테네스의 체 활용
 function isPrime(number){
     let arr = Array(3000).fill(true).fill(false, 0, 2);
     for(let i=2; i<=Math.sqrt(number); i++){
@@ -21,4 +22,33 @@ function solution(nums) {
         }
     }
     return count;
+}
+
+
+
+//or
+
+function primecheck(n){
+    for(var i=2;i<=Math.sqrt(n);i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;    
+}
+function solution(nums){
+    var cnt = 0;
+    for(var i=0;i<nums.length-2;i++){
+        for(var j=i+1;j<nums.length-1;j++){
+            for(var w=j+1;w<nums.length;w++){
+                    //console.log(nums[i]+"/"+nums[j]+"/"+nums[w]);
+
+                    if(primecheck(nums[i]+nums[j]+nums[w])){
+                        //console.log(nums[i]+nums[j]+nums[w]);
+                        cnt++;
+                    }
+            }
+        }
+    }
+    return cnt;
 }
