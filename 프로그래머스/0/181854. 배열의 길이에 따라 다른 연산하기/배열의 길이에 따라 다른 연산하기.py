d3b63d26473b6f0598128adb8fd0e06c910a63ad
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-03-30 20:24:42 +0900

    Update 배열의 길이에 따라 다른 연산하기.py

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -3,3 +3,8 @@ def solution(arr, n):
         return [arr[i] + n if i % 2 == 0 else arr[i] for i in range(len(arr))]
     else:
         return [arr[i] + n if i % 2 == 1 else arr[i] for i in range(len(arr))]
+    
+# 시간복잡도 : O(n) ; n은 arr의 길이
+## 리턴하는 배열 내의 for문이 arr의 길이만큼 돌기 때문에 
+# 공간복잡도 : O(n) ; n은 arr의 길이
+## 리턴하는 배열의 크기가 arr의 길이이기 때문에
