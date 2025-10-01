663542e4dd6715224dae87774486fa78d1ab65a5
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-10-01 10:55:55 +0900

    [Bronze V] Title: A-B, Time: 112 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output:process.stdout
+});
+
+rl.on('line', (line)=>{
+    const [num1, num2]= line.trim().split(" ");
+    console.log(Number(num1) - Number(num2));
+    rl.close();
+})
+
+rl.on('close', ()=>{
+    process.exit();
+})
