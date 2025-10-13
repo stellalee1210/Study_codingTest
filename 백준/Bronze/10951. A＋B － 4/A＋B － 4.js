8c8b04e3858393c2ec24c91592e4c19db16d6d90
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-10-13 10:58:47 +0900

    [Bronze V] Title: A+B - 4, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+let answer = "";
+for (const nums of input) {
+  const [num1, num2] = nums.trim().split(" ").map(Number);
+  answer += num1 + num2 + "\n";
+}
+
+console.log(answer);
