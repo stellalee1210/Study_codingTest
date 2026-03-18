6d278e6868288da55bde4abe61bb15a01dcc7d9f
Stella <133227322+stellalee1210@users.noreply.github.com>
2026-03-18 10:26:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 88 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+const rawNumbers = input[1].split(' ').map(Number).sort((a, b) => a - b);
+const set = new Set(rawNumbers)
+const numbers = [...set]
+
+
+const divisor = numbers.map(number => {
+    if (number <= 1) return 0;
+
+    let divisorCount = 0;
+    for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+        if (number % i === 0) divisorCount += 1;
+    }
+
+    return divisorCount > 0 ? 0 : 1;
+})
+
+const answer = divisor.reduce((acc, cur) => acc + cur)
+console.log(answer)
